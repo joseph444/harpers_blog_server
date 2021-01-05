0c9caa66168784800e7b54eda91a17a6d94128d2
joseph444 <ommakigorom@gmail.com>
2021-01-06 03:30:59 +0530

    debug

--- config/env.js
+++ config/env.js
@@ -1,8 +1,12 @@
-result = require("dotenv").config();
-if(result.error){
-    console.log(result.error);
-    process.exit(-1)
+if(!process.env.PORT){
+    result = require("dotenv").config();
+    if(result.error){
+        console.log(result.error);
+        process.exit(-1)
+    }
 }
+
+
 
 module.exports={
     ...process.env
